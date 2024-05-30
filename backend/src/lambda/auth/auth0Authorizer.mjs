9db44f2f1f58c76a432823be2ff9cf36c81ad220
Walpurgisnacht <minhth.cs2012@gmail.com>
2024-05-30 23:17:16 +0900

    Update src code for debug

--- backend/src/lambda/auth/auth0Authorizer.mjs
+++ backend/src/lambda/auth/auth0Authorizer.mjs
@@ -2,13 +2,37 @@ import Axios from 'axios'
 import jsonwebtoken from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger.mjs'
 
+/* Hard-coded certificate from Auth0 */
+const certificate = `-----BEGIN CERTIFICATE-----
+MIIDHTCCAgWgAwIBAgIJRS/Ve66fbyeFMA0GCSqGSIb3DQEBCwUAMCwxKjAoBgNV
+BAMTIWRldi1jdmMzdjJ0YzIxMWNxeGFmLnVzLmF1dGgwLmNvbTAeFw0yNDA1Mjcx
+MjE2NDFaFw0zODAyMDMxMjE2NDFaMCwxKjAoBgNVBAMTIWRldi1jdmMzdjJ0YzIx
+MWNxeGFmLnVzLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ggEBAMpV42Gd2FKLnToff5JtPvXmbiz10DCL5dA+UASNk7drfL6lRAfLopVMSMGu
+FtG1cKqf00vbS53aFO5Zd2CyaahEv8TMM9c2HWg+/yU6/pf2p8GGVYlJGgQUp6IW
+TNTTYrw6nA/EP9se3xOwG9qqjpvWM3i5IJWI0stqe6MeZxYxPE9Oh8TUvmpWZjVV
+l2uEAdjJSs+eBst4m/2Fc5iHu9VRnaxUEd5lPoPeOOmpo571N3P+t8KfTYVuSScz
+182acAS/IgpTkGoiGE+VsPe64ITJt6s6qKKbrZK7FqVJDSerdjQqcT7nHmerU/ji
+2s91zECS2uDuYWm/aMUFc+GRRG8CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAd
+BgNVHQ4EFgQUkhEHl7AAJFdLxYFqmK7nZy6Mo/owDgYDVR0PAQH/BAQDAgKEMA0G
+CSqGSIb3DQEBCwUAA4IBAQBOlQo7r+aHULshL2vfzldRa5zugkmYXS1GAJmEUp/2
+HNqkzGFk/rZ/yRuMkQe8ovtFvHX5gWdyZbaJG87MZ1G2+CUen3oUvbl7P4zJOxPi
+IKf/8CX0b6kFEAKQZL1HOvYk5s+odi4uzWqRcxUs5v9+7MqddloryMJtnKil4noJ
+8qKc2x/rMwGLHoniKVl9VoR5IjP0oEa7F/sjgvFq9G8TlLufh4K53/jer3joG1nY
+tKxxMcGiGYVvZyrm3yCcNl5vdlx/V/z6/41/Tz1s3218tTyOmDCZjrkfL/3NxVGU
+igCjrtgRWdxOzybcLu4sEuCkR5VBaj7Qq2PeC8taPqQW
+-----END CERTIFICATE-----`
+
 const logger = createLogger('auth')
 
-const jwksUrl = 'https://test-endpoint.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-cvc3v2tc211cqxaf.us.auth0.com/.well-known/jwks.json'
 
 export async function handler(event) {
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
+    logger.info("User was authorized", {
+      userId: jwtToken.sub
+    });
 
     return {
       principalId: jwtToken.sub,
@@ -46,8 +70,9 @@ async function verifyToken(authHeader) {
   const token = getToken(authHeader)
   const jwt = jsonwebtoken.decode(token, { complete: true })
 
-  // TODO: Implement token verification
-  return undefined;
+  // TODO: WIP Implement token verification
+  jsonwebtoken.verify(token, certificate, {algorithms: ['RS256']})
+  return jwt;
 }
 
 function getToken(authHeader) {
--- backend/src/lambda/http/createTodo.js
+++ backend/src/lambda/http/createTodo.js
@@ -1,8 +1,44 @@
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+// import { getUserId } from '../../auth/utils.mjs'
+import { getUserId } from '../utils.mjs';
+import { createTodo } from '../../businessLogic/todos.mjs'
 
-export function handler(event) {
-  const newTodo = JSON.parse(event.body)
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true,
+      origin: '*', // Allow all origins
+      methods: 'GET,PUT,POST,DELETE,OPTIONS', // Allow these methods
+      allowedHeaders: 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token', // Allow these headers
+      preflightContinue: true, // Continue on successful preflight request
+    })
+  )
+  .handler(async (event) => {
+    console.log('Processing event: ', event)
+    const newTodo = JSON.parse(event.body)
+    console.log('newImage', newTodo.name)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
-}
 
+    if (!newTodo.name) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: 'ERROR: The name is empty.'
+        })
+      };
+    }
+
+    const newItem = await createTodo(newTodo, getUserId(event))
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        item: newItem
+      })
+    }
+  })
